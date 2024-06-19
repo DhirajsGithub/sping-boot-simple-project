43cbd03574fe66cd51b52066fb96072960f6a1f4
DhirajsGithub <borsedhiraj123@gmail.com>
2024-06-19 18:15:34 +0530

    ready to deploy

---
+++ demo/src/main/java/com/dhiraj/demo/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.dhiraj.demo.controller;
+
+import com.dhiraj.demo.model.User;
+import com.dhiraj.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<Object> fetchUser(@RequestParam String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
---
+++ demo/src/main/java/com/dhiraj/demo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.dhiraj.demo.repository;
+
+import com.dhiraj.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
