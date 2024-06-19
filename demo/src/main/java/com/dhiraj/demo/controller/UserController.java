12ff1efcec5cdaa6bd65cc1c2eb88c2bb75bb9dc
DhirajsGithub <borsedhiraj123@gmail.com>
2024-06-19 19:24:26 +0530

    sm

--- demo/src/main/java/com/dhiraj/demo/controller/UserController.java
+++ demo/src/main/java/com/dhiraj/demo/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
